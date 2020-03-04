1b480e343dd182016f9a4ab36b491fdd778e5df4
Christoph Tavan <dev@tavan.de>
2020-03-04 14:09:09 +0100

    test: use protractor for browser testing
    Our current browser tests are incredibly flaky and I have no clue why.
    
    It seems like protractor is producing a lot more stable results and it
    seems to offer a few other advantages:
    
    - It seems to handle parallel execution and queuing with browserstack
      correctly.
    - It provides the abstraction for node-based testing that we built on
      our own.

---
+++ test/browser/protractor.conf.js
@@ -0,0 +1,148 @@
+/* global browser:false */
+/* eslint-disable camelcase */
+const util = require('util');
+
+const browserstack = require('browserstack-local');
+const httpServer = require('http-server');
+
+const PORT = 9003;
+const PROJECT = process.env.GITHUB_REPOSITORY || 'node-uuid';
+const GITHUB_SHA = process.env.GITHUB_SHA || '';
+const GITHUB_REF = process.env.GITHUB_REF || '';
+const BUILD = GITHUB_SHA || GITHUB_REF ? `${GITHUB_REF} ${GITHUB_SHA}` : 'manual build';
+
+const commonCapabilities = {
+  project: PROJECT,
+  build: BUILD,
+  name: 'browser test',
+  'browserstack.local': true,
+  'browserstack.debug': false,
+  resolution: '1024x768',
+};
+
+const multiCapabilities = [
+  // Chrome
+  {
+    ...commonCapabilities,
+    browserName: 'Chrome',
+    browser_version: '80.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Chrome',
+    browser_version: '49.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+
+  // Firefox
+  {
+    ...commonCapabilities,
+    browserName: 'Firefox',
+    browser_version: '73.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Firefox',
+    browser_version: '44.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+
+  // Safari
+  {
+    ...commonCapabilities,
+    browserName: 'Safari',
+    browser_version: '13.0',
+    os: 'OS X',
+    os_version: 'Catalina',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Safari',
+    browser_version: '10.0',
+    os: 'OS X',
+    os_version: 'Sierra',
+  },
+
+  // Edge
+  {
+    ...commonCapabilities,
+    browserName: 'Edge',
+    browser_version: '80.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Edge',
+    browser_version: '18.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Edge',
+    browser_version: '15.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+
+  // IE
+  {
+    ...commonCapabilities,
+    browserName: 'IE',
+    browser_version: '11.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'IE',
+    browser_version: '11.0',
+    os: 'Windows',
+    os_version: '7',
+  },
+];
+
+let server;
+let bsLocal;
+
+exports.config = {
+  specs: ['./browser.spec.js'],
+  browserstackUser: process.env.BROWSERSTACK_USER || 'BROWSERSTACK_USERNAME',
+  browserstackKey: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+
+  multiCapabilities,
+  maxSessions: 5, // see https://www.browserstack.com/question/617
+
+  beforeLaunch: async () => {
+    console.log('Starting local http server');
+    server = httpServer.createServer({
+      root: `${__dirname}/../../examples`,
+    });
+    const listen = util.promisify(server.listen).bind(server);
+    await listen(PORT, '0.0.0.0');
+
+    console.log('Connecting local BrowserStack');
+    bsLocal = new browserstack.Local();
+    const start = util.promisify(bsLocal.start).bind(bsLocal);
+    await start({ key: exports.config.browserstackKey });
+
+    console.log('Connected. Now testing...');
+  },
+
+  afterLaunch: async () => {
+    server.close();
+    const stop = util.promisify(bsLocal.stop).bind(bsLocal);
+    await stop();
+  },
+
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  },
+};
