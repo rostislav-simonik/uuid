9cba7b63c03e05edde1393d7b01b0aa7c84899d9
Christoph Tavan <dev@tavan.de>
2020-03-30 16:58:43 +0200

    test: include build

---
+++ dist/esm-node/index.mjs
@@ -0,0 +1,4 @@
+export { default as v1 } from './v1.mjs';
+export { default as v3 } from './v3.mjs';
+export { default as v4 } from './v4.mjs';
+export { default as v5 } from './v5.mjs';
---
+++ dist/esm-node/v3.mjs
@@ -0,0 +1,4 @@
+import v35 from './v35.mjs';
+import md5 from './md5.mjs';
+const v3 = v35('v3', 0x30, md5);
+export default v3;
