b51514c030ad557c2769325ad553acfe1654fad7
Christoph Tavan <dev@tavan.de>
2020-03-30 16:17:24 +0200

    test: naive state extraction

---
+++ dist/esm-node/index.mjs
@@ -0,0 +1,5 @@
+import v1 from './v1.mjs';
+import v3 from './v3.mjs';
+import v4 from './v4.mjs';
+import v5 from './v5.mjs';
+export { v1, v3, v4, v5 };
---
+++ dist/esm-node/v5.mjs
@@ -0,0 +1,4 @@
+import v35 from './v35.mjs';
+import sha1 from './sha1.mjs';
+const v5 = v35('v5', 0x50, sha1);
+export default v5;
